18675799af53578083282dd110b38af14efb6c76
Darren Lester <dsslester@gmail.com>
2016-11-22 15:42:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DCA Movie Scraper
+
+Scrapes all of the movie listings from my favourite local cinema [Dundee Contemporary Arts](http://www.dca.org.uk/)
---
+++ dcascraper.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+# Scrapes and outputs all movies.
+def scrape
+	# create url to retrieve all movies from current date onwards
+	allMoviesURL = "http://www.dca.org.uk/whats-on/films?from=#{Time.now.strftime("%Y-%m-%d")}&to=future"
+
+	# retrieve page
+	page = Nokogiri::HTML(open(allMoviesURL))
+
+	# select the movies
+	movieListingNodes = page.css('.event')
+
+	# parse the movies
+	movieListings = []
+	movieListingNodes.each do |movieListingNode|
+		movieListings << parseMovie(movieListingNode)
+	end
+
+	# output the movies
+	movieListings.each do |movieListing|
+		puts movieListing
+	end
+end
+
+# Converts a movie listing node to a MovieListing object
+def parseMovie(movieListingNode)
+	titleNode = movieListingNode.css('.info')
+	directorNode = movieListingNode.css('.subheading')
+	dateNode = movieListingNode.css('.date')
+
+	title = titleNode[0].text
+	# strip out newline and age rating
+	title = title.gsub(/\n\(.*\)/, '')
+
+	director = directorNode[0].text
+
+	date = dateNode[0].text
+
+	return MovieListing.new(title, director, date)
+end
+
+class MovieListing
+	def initialize(title, director, date)
+		@title = title
+		@director = director
+		@date = date
+	end
+
+	def to_s
+		return "#{@title} - #{@director} (#{@date})"
+	end
+end
+
+scrape
